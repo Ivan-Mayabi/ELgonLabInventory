1dfe914d55691dfc206b678bc3635017210bd0f5
Ivan-Mayabi <mayabivan@gmail.com>
2026-08-07 15:59:38 +0300

    fix ensure the right directories exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Change the permissions of the necessary folders
+mkdir -p storage/framework/sessions storage/framework/views storage/framework/cache storage/logs
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 777 storage bootstrap/cache
 
